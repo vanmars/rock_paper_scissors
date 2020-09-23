ed072f0090378c845438654e5bc01801bd65878e
donnymays <dtmays2@gmail.com>
2020-09-23 11:26:15 -0700

    clean up code

--- rps_script.rb
+++ rps_script.rb
@@ -2,14 +2,11 @@
 
 require ('./lib/rock_paper_scissors')
 
-# Welcome to Rock paper scissors (put)
 puts "******************************************************\n"
 puts "Welcome to the best Rock, Paper, Scissors Game EVER!!!\n"
 puts "******************************************************\n"
 
-# initialize new game
 game = RPS.new()
 
-# game.wins?
 game.play_game() 
 
