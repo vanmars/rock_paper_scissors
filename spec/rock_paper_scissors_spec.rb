6cde12e84b0189c724c3aa8d0e567eb68a6b128a
donnymays <dtmays2@gmail.com>
2020-09-23 10:16:26 -0700

    successful fails of second and third tests

--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -6,5 +6,15 @@ describe('#RPS') do
     game = RPS.new()
     expect(game.wins?("rock", "scissors")).to(eq(true))
   end
+  
+  it("returns false if rock is the object and rock is the argument") do
+    game = RPS.new()
+    expect(game.wins?("rock", "rock")).to(eq(true))
+  end
+  
+  it("returns false if scissors is the object and rock is the argument") do
+    game = RPS.new()
+    expect(game.wins?("scissors", "rock")).to(eq(false))
+  end
 end
 
